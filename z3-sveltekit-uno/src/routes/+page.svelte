6212d8e640b4a09870185399f6ff1a38ec4eb700
Alzera Cita <alzeracita23@gmail.com>
2023-04-17 21:45:15 +0700

    feat: sveltekit-uno

---
+++ z3-sveltekit-uno/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '@unocss/reset/tailwind.css'
+    import 'uno.css'
+</script>
+
+<h1 class="text-blue text-3xl">Welcome to SvelteKit and UnoCss</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="i-mdi-alarm text-3xl text-blue" />
+https://icones.js.org/collection/mdi
